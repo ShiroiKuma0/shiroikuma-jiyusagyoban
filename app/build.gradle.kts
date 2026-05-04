991ad603457d0ee6b389c0da74b3d6fe022bd61a
SysAdminDoc <SysAdminDoc@users.noreply.github.com>
2026-05-04 10:07:44 -0400

    Disable minification and resource shrinking for release builds
    - R8 minification is failing due to Hilt generated class issues
    - Disable temporarily to get working APK for testing
    - Can be re-enabled once code structure is more stable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             // Use debug signing config for testing purposes
             // For Play Store releases, replace with an upload key
