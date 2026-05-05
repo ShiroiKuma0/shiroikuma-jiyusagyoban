ccdd76975e4a43a8ed2ea28bf5eec34ee8eddbda
SysAdminDoc <SysAdminDoc@users.noreply.github.com>
2026-05-05 17:20:31 -0400

    build: upgrade android gradle toolchain

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ val hasReleaseSigning = listOf(
 
 android {
     namespace = "com.opentasker.app"
-    compileSdk = 35
-    buildToolsVersion = "35.0.0"
+    compileSdk = 36
+    buildToolsVersion = "36.0.0"
 
     defaultConfig {
         applicationId = "com.opentasker.app"
