2e2212cc22ae1eb716f33fb4831efe7e8dcf7b57
SysAdminDoc <matt_parker@outlook.com>
2026-06-19 10:56:51 -0400

    chore: bump version to v0.2.75

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ val releaseKeystorePath = System.getenv("OPEN_TASKER_RELEASE_KEYSTORE")
 val releaseKeystorePassword = System.getenv("OPEN_TASKER_RELEASE_KEYSTORE_PASSWORD")
 val releaseKeyAlias = System.getenv("OPEN_TASKER_RELEASE_KEY_ALIAS")
 val releaseKeyPassword = System.getenv("OPEN_TASKER_RELEASE_KEY_PASSWORD")
-val appVersionCode = 76
-val appVersionName = "0.2.74"
+val appVersionCode = 77
+val appVersionName = "0.2.75"
 val allowedDistributions = setOf("standard", "fdroid", "play")
 val selectedDistribution = providers.gradleProperty("openTaskerDistribution")
     .orElse("standard")
